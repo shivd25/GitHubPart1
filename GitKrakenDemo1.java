2757fe5f2f0aa7350c2706bf20492dd3a67266a2
alcumus75 <10020404@sbstudents.org>
2024-11-13 12:14:49 -0500

    Added 1000-thread creation in GitKrakenDemo1 constructor

---
+++ GitKrakenDemo1.java
@@ -0,0 +1,15 @@
+public class GitKrakenDemo1
+{
+	public GitKrakenDemo1()
+	{
+
+
+
+	}
+
+    public static void main(String[] args)
+    {
+        GitKrakenDemo1 app = new GitKrakenDemo1();
+    }
+
+}
